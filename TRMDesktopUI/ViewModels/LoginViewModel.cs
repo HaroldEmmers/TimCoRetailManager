0185f87b77f36f25541adae34bd2d8a7671e8223
HaroldEmmers <harold.emmers@gmail.com>
2019-04-05 21:41:54 +0200

    Add Error Handling to Login
    Added error handling for the login form.

--- TRMDesktopUI/ViewModels/LoginViewModel.cs
+++ TRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -9,6 +9,7 @@ namespace TRMDesktopUI.ViewModels
 	{
 		private string _userName;
 		private string _password;
+		private string _errorMessage;
 		private IApiHelper _apiHelper;
 
 		public LoginViewModel(IApiHelper apiHelper)
@@ -38,6 +39,32 @@ namespace TRMDesktopUI.ViewModels
 			}
 		}
 
+		public bool IsErrorVisible
+		{
+			get
+			{
+				bool output = false;
+				if (ErrorMessage?.Length > 0)
+				{
+					output = true;
+				}
+				return output;
+			}
+		}
+
+		public string ErrorMessage
+		{
+			get { return _errorMessage; }
+			set
+			{
+				_errorMessage = value;
+				NotifyOfPropertyChange(() => ErrorMessage);
+				NotifyOfPropertyChange(() => IsErrorVisible);
+			}
+		}
+
+
+
 		public bool CanLogIn
 		{
 			get
@@ -56,11 +83,12 @@ namespace TRMDesktopUI.ViewModels
 		{
 			try
 			{
+				ErrorMessage = string.Empty;
 				var result = await _apiHelper.Authenticate(UserName, Password);
 			}
 			catch (Exception ex)
 			{
-				Console.WriteLine(ex.Message);
+				ErrorMessage = ex.Message;
 			}
 		}
 	}
